b88831267bb5357b3fdecd6915f2da8a671ef326
bakr kouhadi <kouhadi@bakr.local>
2022-02-19 02:16:37 +0800

    project set up

---
+++ api/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+const app = express();
+import { Users } from "./users.js";
+import cors from "cors";
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  const { q } = req.query;
+
+  const keys = ["first_name", "last_name", "email"];
+
+  const search = (data) => {
+      return data.filter((item) =>
+          keys.some((key) => item[key].toLowerCase().includes(q))
+      );
+    };
+
+
+    q ? res.json(search(Users).slice(0, 20)) : res.json(Users.slice(0, 20));
+
+});
+
+app.listen(5500, () => console.log("Our beautiful API is working!"));
---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import { useEffect, useState, useCallback } from "react";
+import Table from "./Table";
+import axios from "axios";
+
+const App = () => {
+        const [data, setData] = useState([]);
+
+        const fetchData = async (query) => {
+              const res = await axios.get(`http://localhost:5500?q=${query}`);
+              setData(res.data);
+              console.log(query);
+        };
+        // the first fetch
+          useEffect(()=>{
+              fetchData("");
+              console.log('first fetch !');
+          },[]);
+        // the deboucne function 
+        const debounce = (ourFunc) => {
+              let timer;
+              return (...args) => {
+                const context = this;
+                timer && clearTimeout(timer);
+                timer = setTimeout(() =>{
+                      timer=null;
+                      ourFunc.apply(context, args);
+                }, 1000);
+              };
+        };
+        const updateQuery = useCallback(debounce(fetchData),[]);
+       
+  return (
+        <div className="app">
+            <input className="search" placeholder="Search..." onChange={e=>updateQuery(e.target.value.toLowerCase())}/>
+              {<Table data={data} />}
+        </div>
+  );
+}
+export default App;
